23968212b652612508dc8447fb29a46bd0059903
Dan Gibbons <dan.gibbons@rbi.co.uk>
2016-10-13 14:23:24 +0100

    Removing unused varible

--- manifests/chocolatey.pp
+++ manifests/chocolatey.pp
@@ -43,7 +43,6 @@ class windows::chocolatey (
   exec { 'install chocolatey':
     command     => template('windows/chocolately_install.ps1.erb'),
     creates     => $creates,
-    refreshonly => $refreshonly,
     unless      => $unless,
     provider    => powershell,
     timeout     => $timeout,
